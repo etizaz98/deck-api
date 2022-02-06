6b2ecdf43f1385e78985098d39f09ca1f9ad7f1e
Etizaz Ahsan <etizaz@simplusinnovation.com>
2022-02-06 23:05:19 +0500

    test cases added

--- test/documentation/bin.test.doc.ts
+++ test/documentation/bin.test.doc.ts
@@ -9,25 +9,25 @@ describe('Microservices', function(): void {
 
 	describe('katana - Standard: ', function(): void  {
 		it('should return a 200 response', function(done: any): void {
-			request(`http://localhost:${process.env.PORT || 3002}${process.env.BASE_URI || '/api/v1/microservice'}`).get('/ping').then((data) => {
+			request(`http://localhost:${process.env.PORT || 3008}${process.env.BASE_URI || '/api/v1/microservice'}`).get('/ping').then((data) => {
 				expect(data.status).to.equal(200)
 				done()
 			}).catch(err => {done(err)})
 		})
 		it('should return the solution blueprint', function(done: any): void {
-			request(`http://localhost:${process.env.PORT || 3002}${process.env.BASE_URI || '/api/v1/microservice'}`).get('/blueprint').then( data => {
+			request(`http://localhost:${process.env.PORT || 3008}${process.env.BASE_URI || '/api/v1/microservice'}`).get('/blueprint').then( data => {
 				expect(data.status).to.equal(200)
 				done()
 			}).catch(err => {done(err)})
 		})
 		it('should return the solution code documentation', function(done: any): void {
-			request(`http://localhost:${process.env.PORT || 3002}${process.env.BASE_URI || '/api/v1/microservice'}`).get('/documentation').then( data => {
+			request(`http://localhost:${process.env.PORT || 3008}${process.env.BASE_URI || '/api/v1/microservice'}`).get('/documentation').then( data => {
 				expect(data.status).to.equal(200)
 				done()
 			}).catch(err => {done(err)})
 		})
 		it('should return the solution code test suite', function(done: any): void {
-			request(`http://localhost:${process.env.PORT || 3002}${process.env.BASE_URI || '/api/v1/microservice'}`).get('/tests').then( data => {
+			request(`http://localhost:${process.env.PORT || 3008}${process.env.BASE_URI || '/api/v1/microservice'}`).get('/tests').then( data => {
 				expect(data.status).to.equal(200)
 				done()
 			}).catch(err => {done(err)})
